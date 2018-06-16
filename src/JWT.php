d90ead2ea94d13ae02336b0d5907cf919903db53
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-16 11:01:13 +0700

    refactor: use jwt validator trait

--- src/JWT.php
+++ src/JWT.php
@@ -12,6 +12,8 @@ namespace Ahc\Jwt;
  */
 class JWT
 {
+    use ValidatesJWT;
+
     const ERROR_KEY_EMPTY        = 10;
     const ERROR_KEY_INVALID      = 12;
     const ERROR_ALGO_UNSUPPORTED = 20;
@@ -68,7 +70,7 @@ class JWT
      */
     public function __construct($key, string $algo = 'HS256', int $maxAge = 3600, int $leeway = 0, ?string $pass = null)
     {
-        $this->validate($key, $algo, $maxAge, $leeway);
+        $this->validateConfig($key, $algo, $maxAge, $leeway);
 
         if (\is_array($key)) {
             $this->registerKeys($key);
@@ -249,108 +251,5 @@ class JWT
         $this->validateLastJson();
 
         return $data;
-    }
-
-    /**
-     * Throw up if input parameters invalid.
-     *
-     * @codeCoverageIgnore
-     */
-    protected function validate($key, string $algo, int $maxAge, int $leeway)
-    {
-        if (empty($key)) {
-            throw new \InvalidArgumentException('Signing key cannot be empty', static::ERROR_KEY_EMPTY);
-        }
-
-        if (!isset($this->algos[$algo])) {
-            throw new \InvalidArgumentException('Unsupported algo ' . $algo, static::ERROR_ALGO_UNSUPPORTED);
-        }
-
-        if ($maxAge < 1) {
-            throw new \InvalidArgumentException('Invalid maxAge: Should be greater than 0', static::ERROR_INVALID_MAXAGE);
-        }
-
-        if ($leeway < 0 || $leeway > 120) {
-            throw new \InvalidArgumentException('Invalid leeway: Should be between 0-120', static::ERROR_INVALID_LEEWAY);
-        }
-    }
-
-    /**
-     * Throw up if header invalid.
-     */
-    protected function validateHeader(array $header)
-    {
-        if (empty($header['alg'])) {
-            throw new \InvalidArgumentException('Invalid token: Missing header algo', static::ERROR_ALGO_MISSING);
-        }
-        if (empty($this->algos[$header['alg']])) {
-            throw new \InvalidArgumentException('Invalid token: Unsupported header algo', static::ERROR_ALGO_UNSUPPORTED);
-        }
-
-        $this->validateKid($header);
-    }
-
-    /**
-     * Throw up if kid exists and invalid.
-     */
-    protected function validateKid(array $header)
-    {
-        if (!isset($header['kid'])) {
-            return;
-        }
-        if (empty($this->keys[$header['kid']])) {
-            throw new \InvalidArgumentException('Invalid token: Unknown key ID', static::ERROR_KID_UNKNOWN);
-        }
-
-        $this->key = $this->keys[$header['kid']];
-    }
-
-    /**
-     * Throw up if timestamp claims like iat, exp, nbf are invalid.
-     */
-    protected function validateTimestamps(array $payload)
-    {
-        $timestamp = $this->timestamp ?? \time();
-        if (isset($payload['exp']) && $timestamp >= ($payload['exp'] + $this->leeway)) {
-            throw new \InvalidArgumentException('Invalid token: Expired', static::ERROR_TOKEN_EXPIRED);
-        }
-
-        if (isset($payload['iat']) && $timestamp >= ($payload['iat'] + $this->maxAge - $this->leeway)) {
-            throw new \InvalidArgumentException('Invalid token: Expired', static::ERROR_TOKEN_EXPIRED);
-        }
-
-        if (isset($payload['nbf']) && $timestamp <= ($payload['nbf'] - $this->leeway)) {
-            throw new \InvalidArgumentException('Invalid token: Cannot accept now', static::ERROR_TOKEN_NOT_NOW);
-        }
-    }
-
-    /**
-     * Throw up if key is not resource or file path to private key.
-     */
-    protected function validateKey()
-    {
-        if (\is_string($this->key)) {
-            if (!\is_file($this->key)) {
-                throw new \InvalidArgumentException('Invalid key: Should be file path of private key', static::ERROR_KEY_INVALID);
-            }
-
-            $this->key = \openssl_get_privatekey('file://' . $this->key, $this->passphrase ?? '');
-        }
-
-        if (!\is_resource($this->key)) {
-            throw new \InvalidArgumentException('Invalid key: Should be resource of private key', static::ERROR_KEY_INVALID);
-        }
-    }
-
-    /**
-     * Throw up if last json_encode/decode was a failure.
-     */
-    protected function validateLastJson()
-    {
-        if (JSON_ERROR_NONE === \json_last_error()) {
-            return;
-        }
-
-        throw new \InvalidArgumentException('JSON failed: ' . \json_last_error_msg(), static::ERROR_JSON_FAILED);
     }
 }
