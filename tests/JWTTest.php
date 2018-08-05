cba71e0d871c2f2c01d35bea9a0023307483c7bd
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-08-05 09:37:08 +0000

    Apply fixes from StyleCI

--- tests/JWTTest.php
+++ tests/JWTTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PHP-JWT package.
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https://github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
 namespace Ahc\Jwt\Test;
 
 use Ahc\Jwt\JWT;
